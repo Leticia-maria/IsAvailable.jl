c664ea43af9cd43276adfcfc3585d570f5f3d3d7
Letícia Maria Pequeno Madureira <leticiapequeno30@gmail.com>
2021-11-27 09:41:51 -0300

    New version of IsAvailable.jl

--- src/IsAvailable.jl
+++ src/IsAvailable.jl
@@ -1,15 +1,31 @@
 module IsAvailable
 
-function isavailable(name)
-    url = "https://juliapackages.com/p/"*name
+function isavailable(name::String)
+    url = "https://juliapackages.com/p/"*lowercase(name)
     try
-        r = HTTP.get(y)
+        r = HTTP.get(url)
         if r.status == 200
-            print("is available")
+            return true
         end
     catch
-         print("not available")
+        return false
     end
 end 
+
+function app(names::Array{String, 1})
+    for name in names
+        if isavailable(name)
+            print(name*" is available. Do you wanna to install it? [yN] ")
+            answer = readline()
+            if answer == "y"
+                Pkg.add(name)
+            elseif answer == "N"
+                print("Okay... Bye!")
+            else
+                print("Oops! I did it again. Check out the name of the package... It may be wrong")
+            end
+        end
+    end
+end
 
 end # module
