9e40dff85d2b8b2d80c4a211771cef66e130e16d
Leticia-maria <leticiapequeno30@gmail.com>
2021-11-27 20:16:19 -0300

    Documentation Added

--- src/IsAvailable.jl
+++ src/IsAvailable.jl
@@ -1,5 +1,6 @@
 module IsAvailable
 
+@doc """ A function that gets the status of a package at the link https://juliapackages.com/p/ """ ->
 function isavailable(name::String)
     url = "https://juliapackages.com/p/"*lowercase(name)
     try
@@ -12,17 +13,25 @@ function isavailable(name::String)
     end
 end 
 
+@doc """ A function that gets the status of several packages in a list""" ->
 function app(names::Array{String, 1})
     for name in names
         if isavailable(name)
             print(name*" is available. Do you wanna to install it? [type y(yes) or n(no)] ")
             answer = readline()
-            if lowercase(answer) == "y"
-                Pkg.add(name)
-            elseif lowercase(answer) == "n"
+            validAnswers = ["yes", "no", "y", "n"]
+            while lowercase(answer) ∉ validAnswers
+                print("Please, type some valid option: [y] yes and [n] no. ")
+                answer = readline()
+            end
+            if lowercase(answer) ∈ ["yes", "y"]
+                try 
+                    Pkg.add(name)
+                catch
+                    print("Oops! I did it again. Check out the name of the package... It may be wrong")
+                end
+            elseif lowercase(answer) ∈ ["no", "n"]
                 print("Okay... Bye!")
-            else
-                print("Oops! I did it again. Check out the name of the package... It may be wrong")
             end
         end
     end
